4b43287650b839007f13edd4df1eff37b9fa75bc
Surya Prakash Yadav <suryaprakash8707yadav@gmail.com>
2026-01-07 00:12:00 +0530

    Objects in Java

--- Objects/Main.java
+++ Objects/Main.java
@@ -1,20 +1,16 @@
 class Student {
     int id;
     String name;
-
     void display() {
         System.out.println("ID: " + id + ", Name: " + name);
     }
 }
-
 public class Main {
     public static void main(String[] args) {
         Student s1 = new Student();   
         Student s2 = new Student();   
-
         s1.id = 101;
         s1.name = "Surya";
-
         s2.id = 102;
         s2.name = "Aman";
 
